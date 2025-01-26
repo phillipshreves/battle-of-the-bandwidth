0ba7105da02bcbf65d5ba82e9a038d18e52728ef
Phillip Shreves <phillipshreves@gmail.com>
2025-01-26 11:05:13 -0700

    Initial node setup

--- backend-node/src/main.ts
+++ backend-node/src/main.ts
@@ -1,10 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
 import helmet from 'helmet';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -31,53 +28,6 @@ async function bootstrap() {
     }),
   );
 
-  app.use(cookieParser());
-
-  const configService = app.get(ConfigService);
-
-  if (configService.get('devCORSenabled') === 'true') {
-    app.enableCors({
-      origin: [
-        'http://192.168.64.2:3002',
-        'http://192.168.64.2:3000',
-        'http://192.168.64.2:3003',
-      ],
-      credentials: true,
-    });
-  }
-
-  if (configService.get('prodCORSenabled') === 'true') {
-    app.enableCors({
-      origin: [
-        'https://internal-portal-frontend-web-next.vercel.app',
-        'https://internal.highpointgo.com',
-        'https://developer-portal-frontend-web-next.vercel.app',
-      ],
-      credentials: true,
-    });
-  }
-
-  const port = process.env.PORT || 3000;
-
-  const swaggerConfig = new DocumentBuilder()
-    .addServer(configService.get('openapi.server.address'))
-    .setTitle('Authenticator Service API')
-    .setDescription(
-      'Developer documentation for High Point Authentication Service.',
-    )
-    .addTag('token')
-    .addTag('user')
-
-    .setVersion('1.0')
-    .addBearerAuth({
-      type: 'http',
-      description:
-        'First call the login endpoint, then add the JWT from sign-in response into this input',
-    })
-    .build();
-  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('docs', app, swaggerDocument);
-
-  await app.listen(port);
+  await app.listen(3000);
 }
 bootstrap();
